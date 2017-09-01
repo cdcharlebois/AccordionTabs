06544b3aa5f3796bc4daa374e61fe9c4b303c590
Conner Charlebois <cdcharlebois@gmail.com>
2017-09-01 16:17:52 -0400

    tab accordion

---
+++ src/TabAccordion/widget/TabAccordion.js
@@ -0,0 +1,112 @@
+define([
+    "dojo/_base/declare",
+    "mxui/widget/_WidgetBase",
+
+    "mxui/dom",
+    "dojo/dom",
+    "dojo/dom-prop",
+    "dojo/dom-geometry",
+    "dojo/dom-class",
+    "dojo/dom-style",
+    "dojo/dom-construct",
+    "dojo/_base/array",
+    "dojo/_base/lang",
+    "dojo/text",
+    "dojo/html",
+    "dojo/_base/event"
+
+
+], function(declare, _WidgetBase, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml, dojoEvent) {
+    "use strict";
+
+    return declare("TabAccordion.widget.TabAccordion", [_WidgetBase], {
+
+        breakpoint: null,
+        accordionClass: null,
+        // Internal variables.
+        _handles: null,
+        _contextObj: null,
+
+        constructor: function() {
+            this._handles = [];
+        },
+
+        postCreate: function() {
+            logger.debug(this.id + ".postCreate");
+            this.ul = this.domNode.parentElement.querySelector('ul');
+            this.tabs = this.ul.children;
+            this.tabContent = this.ul.parentElement.querySelector('.tab-content.mx-tabcontainer-content');
+
+            this._attachCSS();
+        },
+
+        update: function(obj, callback) {
+            logger.debug(this.id + ".update");
+
+            this._contextObj = obj;
+            this._updateRendering(callback);
+            this._setInitialTab();
+        },
+
+        resize: function(box) {
+            logger.debug(this.id + ".resize");
+            if (window.innerWidth >= this.breakpoint) {
+                //reset the DOM
+                this.ul.parentElement.appendChild(this.tabContent);
+                Array.from(this.tabs).forEach(lang.hitch(this, function(tab) {
+                    dojoClass.remove(tab, this.accordionClass)
+                }))
+            } else {
+                //move the tabContent to the active tab
+                var activeTab = Array.from(this.tabs).find(function(e) { return e.className.indexOf('active') > -1 })
+                this.ul.insertBefore(this.tabContent, activeTab.nextElementSibling)
+                Array.from(this.tabs).forEach(lang.hitch(this, function(tab) {
+                    dojoClass.add(tab, this.accordionClass)
+                }))
+            }
+        },
+
+        uninitialize: function() {
+            logger.debug(this.id + ".uninitialize");
+        },
+
+        _attachCSS: function() {
+            var cssText = `
+                @media screen and (max-width: ${this.breakpoint}px){
+                    #${this.ul.parentElement.id} > ul > li {clear: both; width: 100%;}
+                }        
+            `;
+            var css = document.createElement("style");
+            css.type = "text/css";
+            css.innerHTML = cssText;
+            document.body.appendChild(css);
+        },
+
+        _updateRendering: function(callback) {
+            logger.debug(this.id + "._updateRendering");
+
+            Array.from(this.tabs).forEach(lang.hitch(this, function(el) {
+                el.addEventListener('click', lang.hitch(this, this.__handleAccordionClick))
+            }));
+            this._executeCallback(callback);
+        },
+
+        _setInitialTab: function() {
+            this.ul.insertBefore(this.tabContent, this.tabs[0].nextElementSibling)
+        },
+
+        __handleAccordionClick: function(e) {
+            if (window.innerWidth < this.breakpoint) {
+                e.target.parentElement.insertBefore(this.tabContent, e.target.nextElementSibling)
+            }
+        },
+
+        _executeCallback: function(cb) {
+            if (cb && typeof cb === "function") {
+                cb();
+            }
+        }
+    });
+});
+
+require(["TabAccordion/widget/TabAccordion"]);
